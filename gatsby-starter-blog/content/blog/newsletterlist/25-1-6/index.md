b3f7c5a94f002d61c91f884d359e320adaf5a429
Alec Harrison <aah1@mac.com>
2025-01-11 22:21:30 -0600

    Adding newsletter

---
+++ gatsby-starter-blog/content/blog/newsletterlist/25-1-6/index.md
@@ -0,0 +1,159 @@
+---
+title: Newsletter 25-1-6
+date: "2025-01-07T22:10:03.284Z"
+description: This entry is my goal to explain why I have created a newsletter, what I plan to include in it, and what you should expect from it!
+---
+
+<div class="newsletter-content">
+
+# Azure Barista Newsletter 1st Edition!
+
+![Azure Barista Logo](https://stratus.campaign-image.com/images/1389757000000070614_zc_v1_1736225420747_azurebarista.jpg)
+
+## Hey there!
+
+Welcome to the first edition of Azure Barista Newsletter!
+
+### Who Am I?
+
+![Profile Photo](https://stratus.campaign-image.com/images/1389757000000070614_zc_v1_1736226263861_me.jpeg)
+
+I am Alec Harrison, a Microsoft MVP in AI and Azure, a software development enthusiast passionate about test-driven development, cloud technologies, and agile methodologies. In my free time you'll find me learning about new Azure technologies and helping to running a few user groups!
+
+### What's the reason for this newsletter?
+
+This newsletter is more than just sharing updates—it's about creating a community where knowledge fuels innovation. With the rapid evolution of technology, staying informed can be overwhelming. This newsletter will bridge the gap, delivering the most important news, tips, and trends in a concise, approachable way. By curating insights and actionable content, I will empower readers to harness Azure's potential, make informed decisions, and stay ahead in the cloud landscape. This isn't just a newsletter; it's a fresh brew of inspiration for professionals ready to elevate their skills and transform their organizations.
+
+I am also hoping this will also be a one stop shop for anyone who wants to keep up with what I am working on!
+
+### Join me on Meetup! Most events are virtual friendly!
+
+Next event is being announced soon it will be on 1/14/25!
+
+### Recent Activity
+</div>
+
+#### Featured Videos:
+<div class="row">
+	<div class="column">
+		<h5> Interview w Tim </h5>
+		<iframe src="https://www.youtube.com/embed/qxDJsPgmZIw?si=h0659Wb4v3ld2FjE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+	</div>
+	<div class="column">
+		<h5> Deploying Private LLMs </h5>
+		<iframe src="https://www.youtube.com/embed/dbYDQ1FbIVs?si=LazenLtRKDZm-joE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+	</div>
+	<div class="column">
+		<h5> Interview with Clark Sell </h5>
+		<iframe src="https://www.youtube.com/embed/0p4hDpVyPHU?si=4o_8N-_m05s6wVVi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+	</div>
+</div>
+
+<div class="newsletter-content">
+<h4> Highlight worthy blog posts! </h4>
+
+#### Which Microsoft AI is Right For Me?
+There are currently 3 main workflows in Microsoft's ecosystem for AI. They are: Azure Open AI/Azure AI Studio (which just went GA), Azure Machine Learning, and Microsoft Copilot. Each of these has their own unique features and use cases, so let's dive into each one to help you decide...
+
+[Read More](https://www.azurebarista.com/which-ai-is-for-me/)
+
+#### Is AI the Right Solution For My Problem?
+In today's tech-driven world, artificial intelligence (AI) is often hailed as the magic solution to all of our problems. But let's be real—AI isn't always the answer. It's easy to get swept up in the hype, only to end up using a bulldozer when all you really needed was a shovel.
+
+As a technical consultant and Microsoft MVP in Azure and AI, I work with companies every day to explore how...
+
+[Read More](https://leantechniques.com/2024/10/04/is-ai-the-right-solution-for-my-problem-ai-decisioning-flowchart-tool/)
+
+### Follow Us On
+
+<div class="social-links">
+  <a href="https://x.com/Alec13355" class="social-link">
+    <img src="https://stratus.campaign-image.com/images/1389757000000070614_1_1736229290483_zcsclwgttwt2.png" alt="Twitter" />
+    <span>Twitter</span>
+  </a>
+  <a href="https://www.linkedin.com/in/alec13355/" class="social-link">
+    <img src="https://stratus.campaign-image.com/images/1389757000000070614_2_1736229290622_zcsclwgtlin2.png" alt="LinkedIn" />
+    <span>LinkedIn</span>
+  </a>
+  <a href="https://www.youtube.com/@imaug-iowamicrosoftazureus6005" class="social-link">
+    <img src="https://stratus.campaign-image.com/images/1389757000000070614_3_1736229290666_zcsclwgtyt2.png" alt="Azure Universe" />
+    <span>Azure Universe</span>
+  </a>
+  <a href="https://feeds.simplecast.com/VN2wnFJq" class="social-link">
+    <img src="https://stratus.campaign-image.com/images/1389757000000070614_4_1736229290708_zcsclwgtrss2.png" alt="Azure Cloud Talk" />
+    <span>Azure Cloud Talk</span>
+  </a>
+  <a href="mailto:hello@xerikos.com" class="social-link">
+    <img src="https://stratus.campaign-image.com/images/1389757000000070614_5_1736229290745_zcsclwgtmail2.png" alt="Email" />
+    <span>Email</span>
+  </a>
+</div>
+
+</div>
+
+<style>
+.newsletter-content {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.newsletter-content img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+  margin: 20px auto;
+}
+
+@media screen and (max-width: 800px) {
+  .column {
+    width: 100%;
+  }
+}
+
+@media screen and (min-width: 801px){
+	.column {
+		float: left;
+		width: 33.33%;
+	}
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.social-links {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin: 20px 0;
+  background-color: black;
+  padding: 20px;
+  border-radius: 8px;
+}
+
+.social-link {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
+  color: #ffffff;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 12px;
+}
+
+.social-link img {
+  width: 30px;
+  height: 30px;
+  margin-bottom: 6px;
+}
+
+.social-link span {
+  text-align: center;
+}
+
+</style>
--- gatsby-starter-blog/src/components/layout.js
+++ gatsby-starter-blog/src/components/layout.js
@@ -1,10 +1,14 @@
 import * as React from "react"
-import { Link, Script } from "gatsby"
+import { Link } from "gatsby"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
+
+  const referenceSetter = (element) => {
+    console.log('Image loaded:', element);
+  };
 
   if (isRootPath) {
     header = (
@@ -19,18 +23,269 @@ const Layout = ({ location, title, children }) => {
       </Link>
     )
   }
-
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       
-      <script type="text/javascript" src="https://campaigns.zoho.com/js/zc.iframe.js"></script>
-      <iframe frameborder="0" id="iframewin" width="100%" height="100%" src="https://zcsub-cmpzourl.maillist-manage.com/ua/Optin?od=11287eccd8a904&zx=12dbe12b6&tD=1134968a0d51d87a9&sD=1134968a0d51d898a"></iframe>
+      {/* <div id="newsletter-container">
+        <div id="sf3zc9b642d99270ab0e0d58ca830ea795655b103455b7d50f6335671bfa4eefc8bb" data-type="signupform">
+          <div id="customForm">
+            <div className="quick_form_18_css" 
+              style={{
+                backgroundColor: "rgb(255, 255, 255)",
+                width: "300px",
+                zIndex: 2,
+                fontFamily: "Arial",
+                border: "1px solid rgb(206, 206, 206)",
+                overflow: "hidden"
+              }} 
+              name="SIGNUP_BODY"
+            >
+              <div style={{ textAlign: "center" }}>
+                <div style={{
+                  fontSize: "22px",
+                  fontFamily: "Arial",
+                  fontWeight: "normal",
+                  color: "rgb(1, 176, 255)",
+                  textAlign: "left",
+                  padding: "15px",
+                  width: "100%",
+                  display: "block",
+                  backgroundColor: "transparent"
+                }} id="SIGNUP_HEADING">
+                  Join Our Newsletter
+                </div>
+                <div style={{ position: "relative" }}>
+                  <div id="Zc_SignupSuccess" style={{
+                    display: "none",
+                    position: "absolute",
+                    marginLeft: "4%",
+                    width: "90%",
+                    backgroundColor: "white",
+                    padding: "3px",
+                    border: "3px solid rgb(194, 225, 154)",
+                    marginTop: "10px",
+                    marginBottom: "10px",
+                    wordBreak: "break-all"
+                  }}>
+                    <table width="100%" cellPadding="0" cellSpacing="0" border="0">
+                      <tbody>
+                        <tr>
+                          <td width="10%">
+                            <img 
+                              className="successicon" 
+                              src="https://zcsub-cmpzourl.maillist-manage.com/images/challangeiconenable.jpg" 
+                              alt="success icon"
+                            />
+                          </td>
+                          <td>
+                            <span id="signupSuccessMsg" style={{
+                              color: "rgb(73, 140, 132)",
+                              fontFamily: "sans-serif",
+                              fontSize: "14px",
+                              wordBreak: "break-word"
+                            }}>
+                              &nbsp;&nbsp;Thank you for Signing Up
+                            </span>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+                <form 
+                  method="POST" 
+                  id="zcampaignOptinForm" 
+                  style={{ margin: 0, width: "100%" }} 
+                  action="https://zcsub-cmpzourl.maillist-manage.com/weboptin.zc" 
+                  target="_zcSignup"
+                >
+                  <div style={{
+                    backgroundColor: "rgb(255, 235, 232)",
+                    padding: "10px",
+                    color: "rgb(210, 0, 0)",
+                    fontSize: "11px",
+                    margin: "20px 10px 0px",
+                    border: "1px solid rgb(255, 217, 211)",
+                    opacity: 1,
+                    display: "none"
+                  }} id="errorMsgDiv">
+                    Please correct the marked field(s) below.
+                  </div>
+                  <div style={{
+                    position: "relative",
+                    width: "270px",
+                    height: "40px",
+                    marginTop: "15px",
+                    display: "inline-block"
+                  }}>
+                    <input 
+                      type="text" 
+                      style={{
+                        fontSize: "14px",
+                        borderWidth: "0 0 2px",
+                        borderColor: "rgb(1, 176, 255)",
+                        borderStyle: "solid",
+                        width: "100%",
+                        height: "100%",
+                        zIndex: 4,
+                        outline: "none",
+                        padding: "5px 10px",
+                        boxSizing: "border-box",
+                        color: "rgb(0, 0, 0)",
+                        textAlign: "left",
+                        fontFamily: "Arial",
+                        backgroundColor: "rgb(255, 255, 255)",
+                        borderRadius: "0px"
+                      }}
+                      placeholder="Email"
+                      changeitem="SIGNUP_FORM_FIELD"
+                      name="CONTACT_EMAIL"
+                      id="EMBED_FORM_EMAIL_LABEL"
+                    />
+                  </div>
+                  <div style={{
+                    position: "relative",
+                    width: "270px",
+                    height: "40px",
+                    marginTop: "30px",
+                    display: "inline-block"
+                  }}>
+                    <input 
+                      type="text" 
+                      style={{
+                        fontSize: "14px",
+                        borderWidth: "0 0 2px",
+                        borderColor: "rgb(1, 176, 255)",
+                        borderStyle: "solid",
+                        width: "100%",
+                        height: "100%",
+                        zIndex: 4,
+                        outline: "none",
+                        padding: "5px 10px",
+                        boxSizing: "border-box",
+                        color: "rgb(0, 0, 0)",
+                        textAlign: "left",
+                        fontFamily: "Arial",
+                        backgroundColor: "rgb(255, 255, 255)",
+                        borderRadius: "0px"
+                      }}
+                      placeholder="Name"
+                      changeitem="SIGNUP_FORM_FIELD"
+                      name="LASTNAME"
+                      id="EMBED_FORM_NAME_LABEL"
+                    />
+                  </div>
+                  <div style={{
+                    position: "relative",
+                    width: "150px",
+                    height: "40px",
+                    margin: "20px 115px 25px auto",
+                    display: "inline-block"
+                  }}>
+                    <input 
+                      type="button" 
+                      style={{
+                        textAlign: "center",
+                        width: "100%",
+                        height: "100%",
+                        zIndex: 5,
+                        border: 0,
+                        color: "rgb(255, 255, 255)",
+                        cursor: "pointer",
+                        outline: "none",
+                        fontSize: "14px",
+                        backgroundColor: "rgb(1, 176, 255)",
+                        borderRadius: "5px"
+                      }}
+                      name="SIGNUP_SUBMIT_BUTTON"
+                      id="zcWebOptin"
+                      value="Join Now"
+                    />
+                  </div>
+                  <input type="hidden" id="fieldBorder" value=""/>
+                  <input type="hidden" id="submitType" name="submitType" value="optinCustomView"/>
+                  <input type="hidden" id="emailReportId" name="emailReportId" value=""/>
+                  <input type="hidden" id="formType" name="formType" value="QuickForm"/>
+                  <input type="hidden" name="zx" id="cmpZuid" value="12dbe12b6"/>
+                  <input type="hidden" name="zcvers" value="3.0"/>
+                  <input type="hidden" name="oldListIds" id="allCheckedListIds" value=""/>
+                  <input type="hidden" id="mode" name="mode" value="OptinCreateView"/>
+                  <input type="hidden" id="zcld" name="zcld" value="1134968a0d51d88f4"/>
+                  <input type="hidden" id="zctd" name="zctd" value="1134968a0d51d87a9"/>
+                  <input type="hidden" id="document_domain" value=""/>
+                  <input type="hidden" id="zc_Url" value="zcsub-cmpzourl.maillist-manage.com"/>
+                  <input type="hidden" id="new_optin_response_in" value="0"/>
+                  <input type="hidden" id="duplicate_optin_response_in" value="0"/>
+                  <input type="hidden" name="zc_trackCode" id="zc_trackCode" value="ZCFORMVIEW"/>
+                  <input type="hidden" id="zc_formIx" name="zc_formIx" value="3zc9b642d99270ab0e0d58ca830ea795655b103455b7d50f6335671bfa4eefc8bb"/>
+                  <input type="hidden" id="viewFrom" value="URL_ACTION"/>
+                  <span style={{ display: "none" }} id="dt_CONTACT_EMAIL">1,true,6,Contact Email,2</span>
+                  <span style={{ display: "none" }} id="dt_FIRSTNAME">1,false,1,First Name,2</span>
+                  <span style={{ display: "none" }} id="dt_LASTNAME">1,false,1,Last Name,2</span>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img 
+          src="https://zcsub-cmpzourl.maillist-manage.com/images/spacer.gif" 
+          id="refImage" 
+          onLoad={() => referenceSetter(this)}
+          style={{display: "none"}}
+          alt=""
+        />
+        <input type="hidden" id="signupFormType" value="QuickForm_Vertical" />
+        <div id="zcOptinOverLay" 
+          onContextMenu={() => false} 
+          style={{
+            display: "none",
+            textAlign: "center",
+            backgroundColor: "rgb(0, 0, 0)",
+            opacity: 0.5,
+            zIndex: 100,
+            position: "fixed",
+            width: "100%",
+            top: 0,
+            left: 0,
+            height: "988px"
+          }}
+        />
+        <div id="zcOptinSuccessPopup" 
+          style={{
+            display: "none",
+            zIndex: 9999,
+            width: "800px",
+            height: "40%",
+            top: "84px",
+            position: "fixed",
+            left: "26%",
+            backgroundColor: "#FFFFFF",
+            borderColor: "#E6E6E6",
+            borderStyle: "solid",
+            borderWidth: "1px",
+            boxShadow: "0 1px 10px #424242",
+            padding: "35px"
+          }}
+        >
+          <span 
+            style={{
+              position: "absolute",
+              top: "-16px",
+              right: "-14px",
+              zIndex: 99999,
+              cursor: "pointer"
+            }} 
+            id="closeSuccess"
+          >
+            <img 
+              src="https://zcsub-cmpzourl.maillist-manage.com/images/videoclose.png"
+              alt="close"
+            />
+          </span>
+          <div id="zcOptinSuccessPanel"></div>
+        </div>
+      </div> */}
 
-
- 
-
-
-    
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
@@ -39,7 +294,7 @@ const Layout = ({ location, title, children }) => {
         <a href="https://www.gatsbyjs.com">Gatsby</a>
       </footer>
     </div>
-  )
+  );
 }
 
 export default Layout
